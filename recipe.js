23ef84849510180e2b399d5561d3a990f20c0f09
chasem <cpmcquown@gmail.com>
2021-06-19 11:03:30 -0700

    css confirmed

--- recipe.js
+++ recipe.js
@@ -16,3 +16,70 @@ async function sendApiRequest(){
 function useApiData (data){
     console.log(data)
 }
+
+
+/* 
+
+Chase's JS
+
+// reference to search button element
+var searchButton = document.querySelector("#search-btn");
+
+// api key 
+var apiKey = 45d6a42a7bd9410a991b8738069e8917
+
+// function that will generate movie and recipe
+var generate = function() {
+
+    // ref to recipe value
+    var recipeGenre = document.querySelector("#food-selection").value;
+
+    // ref to movie value
+    var movieGenre = document.querySelector("#movie-selection").value; 
+
+    // fetch recipe
+    fetch(
+        "https://api.spoonacular.com/recipes/search? + apiKey + recipeGenre
+    )
+    .then(funcion(response) {
+
+        // if response is successful
+        if (response.ok) {
+            console.log("success")
+        }else {
+            console.log("unsuccessful")
+        }
+
+        return response.json()
+
+    })
+    .then(function(data) {
+
+        // this is where recipe will be displayed to page
+
+    })
+
+    // fetch movie
+    fetch(
+        "https://" + movieGenre
+    )
+    .then(fucntion(response) {
+        
+        return response.json();
+
+    })
+    .then(function(data) {
+
+        //this is where movie will be displayed to page
+
+    })
+
+
+
+
+};
+
+// add event listener to search button, then call generate function
+searchButton.addEventListener("click", generate);
+
+*/
